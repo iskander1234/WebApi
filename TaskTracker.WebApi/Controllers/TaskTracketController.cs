c07344e708f61c5fe52f7b685749d2914bbef5cd
Iskander Dosgali <idosgali@gmail.com>
2022-02-19 17:00:38 +0600

    initial commit

---
+++ TaskTracker.WebApi/Controllers/TaskTracketController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaskTracker.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskTrackerController : ControllerBase
+    {
+        public TaskTrackerController()
+        {
+        }
+    }
+}
